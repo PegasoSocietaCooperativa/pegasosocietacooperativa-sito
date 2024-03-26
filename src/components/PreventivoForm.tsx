9d3b99533501ed18e27b168993f1cb0d66628808
Ludovico Colucci <ludovicocolucci@gmail.com>
2024-03-26 12:03:12 +0100

    [ADD] mail service and ref style

---
+++ src/components/PreventivoForm.tsx
@@ -0,0 +1,36 @@
+import emailjs from 'emailjs-com';
+import '../styles/PreventivoForm.css'; // Assicurati di importare lo stile coerente con il resto del sito
+
+const PreventivoForm = () => {
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+        const serviceID = 'service_qnmzx4m';
+        const templateID = 'template_xh8wekz';
+        const userID = 'fkHEG6re4cbWnCToq';
+
+        emailjs.sendForm(serviceID, templateID, e.target, userID).then(
+            (response) => {
+                alert('Richiesta Inviata!');
+            },
+            (error) => {
+                alert(`C'è stato un errore..., ${error.message}`);
+            },
+        );
+
+        e.target.reset();
+    };
+
+    return (
+        <>
+            <form className="preventivoForm" onSubmit={sendEmail}>
+                {/* <input className="formInput" type="text" name="reply_to" placeholder="Your Name" required/> */}
+                <input className="formInput" type="email" name="from_name" placeholder="Inserisci la tua mail..." required />
+                <textarea className="formTextarea" name="message" placeholder="Scrivi qui il tuo mesaggio..." required></textarea>
+                <button className="formButton" type="submit">Invia Richiesta</button>
+            </form>
+
+        </>
+    );
+}
+
+export default PreventivoForm;
--- src/pages/Services/Giardinaggio.tsx
+++ src/pages/Services/Giardinaggio.tsx
@@ -1,16 +1,26 @@
+import React from 'react';
 import '../../styles/Giardinaggio.css'; // Assicurati di importare lo stile coerente con il resto del sito
+import PreventivoForm from '../../components/PreventivoForm';
+import giardinaggio4Img from '../../assets/services/giardinaggio4.jpg';
 
 const Giardinaggio = () => {
     return (
         <div className="giardinaggioPageContainer">
-            {/* AGGIUNGERE HERO CARD DEL SERVIZIO */}
-            {/* AGGIUNGERE "RICHIEDI ORA UN PREVENTIVO GRATUITO" */}
+            {/* HERO CARD DEL SERVIZIO */}
+            <div className="heroCard">
+                {/* Contenuto della Hero Card */}
+                <img src={giardinaggio4Img} alt="Servizio Giardinaggio" className="serviceImage" />
+            </div>
+
+
             <h1 className="giardinaggioPageTitle">Servizi di Giardinaggio</h1>
             <div className="giardinaggioPageContent">
                 <p>PEGASO SOCIETA’ COOPERATIVA si occupa sia di manutenzione ordinaria regolare che di quella straordinaria.</p>
                 <p>Si impegna a mantenere il giardino del cliente in perfette condizioni nel tempo.</p>
                 <p>Si effettuano sfalci, pulizie dei manti erbosi, potature siepi e tanto altro, tutto ciò utilizzando metodi e strumenti professionali con il nostro personale competente e qualificato, pronto a soddisfare ogni tipologia di clientela, dal pubblico al privato.</p>
                 <p>Contattaci per chiedere un preventivo con sopralluogo gratuito!</p>
+                <PreventivoForm />
+
             </div>
         </div>
     );
